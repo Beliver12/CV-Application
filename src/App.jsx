e951ff7949d4b1d26c1af5a72e1500b52a63c5a7
Nikola <nzencic@gmail.com>
2024-02-22 13:28:45 +0100

    made general info

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,79 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+const [firstName, setFirstName] = useState('')
+const [lastName, setLastName] = useState('')
+const [mail, setMail] = useState('')
+const [phoneNumber, setPhoneNumber] = useState('')
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <div id='container'>
+<section className='General-section'>
+<h1>General Info</h1>
+<form className='General-inputs'>
+  <div>
+    <FullName label="First-Name" handleChange={(e) => setFirstName(e.target.value)} value={firstName}/>
+    <FullName label="Last-Name" handleChange={(e) => setLastName(e.target.value)} value={lastName}/>
+  </div>
+  <div>
+    <Email label="E-mail" handleChange={(e) => setMail(e.target.value)} value={mail}/>
+  </div>
+  <div>
+    <PhoneNumber label="Phone-number" handleChange={(e) => setPhoneNumber(e.target.value)} value={phoneNumber}/>
+  </div>
+  <button>Submit</button>
+</form>
+<div className='General-info'>
+<p>First-Name: {firstName}</p>
+<p>Last-Name {lastName}</p>
+<p>E-mail: {mail}</p>
+<p>Phone: {phoneNumber} </p>
+</div>
+</section>
+   </div>
+
+  )
+}
+
+function FullName({label, handleChange, value}) {
+  return (
+    <label>
+      {label}
+      {''}
+      <input type="text" 
+      value={value}
+      onChange={handleChange}
+      />
+     
+    </label>
+  )
+}
+
+function Email({label, handleChange, value}) {
+  return (
+    <label>
+      {label}
+      {''}
+      <input type="email" 
+      value={value}
+      onChange={handleChange}
+      />
+     
+    </label>
+  )
+}
+
+function PhoneNumber({label, handleChange, value}) {
+  return (
+    <label>
+      {label}
+      {''}
+      <input type="tel" 
+      value={value}
+      onChange={handleChange}
+      />
+     
+    </label>
   )
 }
 
