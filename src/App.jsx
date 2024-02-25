4ab1150bdfdaaf6aa50496e8b166141e4bb430c1
Nikola <nzencic@gmail.com>
2024-02-25 13:26:48 +0100

    ad

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,49 @@ const [firstName, setFirstName] = useState('')
 const [lastName, setLastName] = useState('')
 const [mail, setMail] = useState('')
 const [phoneNumber, setPhoneNumber] = useState('')
+const [generalInfo, seGeneralInfo] = useState({firstName: '',lastName: '', mail:'', phoneNumber:''})
 
 const [schoolName, setSchoolName] = useState('')
 const [titleOfStudy, settitleOfStudy] = useState('')
 const [dateOfStudy, setdateOfStudy] = useState('')
 const [educationInfo, setEducationInfo] = useState({name: '', title: '', date: ''})
 
-function handleSubmit(e) {
+function handleEducationalSubmit(e) {
   e.preventDefault()
   setEducationInfo({...educationInfo, name: schoolName, title: titleOfStudy, date: dateOfStudy})
+  setSchoolName('')
+  settitleOfStudy('')
+  setdateOfStudy('')
+}
 
+function handleEducationalEdit(){
+  setSchoolName(educationInfo.name)
+  settitleOfStudy(educationInfo.title)
+  setdateOfStudy(educationInfo.date)
+}
+
+function handleGeneralEdit(){
+  setFirstName(generalInfo.firstName)
+  setLastName(generalInfo.lastName)
+  setMail(generalInfo.mail)
+  setPhoneNumber(generalInfo.phoneNumber)
+}
+
+
+function handleGeneralSubmit(e) {
+ e.preventDefault();
+ seGeneralInfo({...generalInfo, firstName: firstName , lastName: lastName, mail: mail, phoneNumber: phoneNumber})
+ setFirstName('')
+ setLastName('')
+ setMail('')
+ setPhoneNumber('')
 }
 
   return (
    <div id='container'>
 <section className='General-section'>
-<h1>General Info</h1>
-<form className='General-inputs'>
+<h1>General Information</h1>
+<form className='General-inputs' onSubmit={handleGeneralSubmit}>
   <div>
     <FullName label="First-Name" handleChange={(e) => setFirstName(e.target.value)} value={firstName}/>
     <FullName label="Last-Name" handleChange={(e) => setLastName(e.target.value)} value={lastName}/>
@@ -38,23 +64,26 @@ function handleSubmit(e) {
   <button>Submit</button>
 </form>
 <div className='General-info'>
-<p>First-Name: {firstName}</p>
-<p>Last-Name {lastName}</p>
-<p>E-mail: {mail}</p>
-<p>Phone: {phoneNumber} </p>
+<p>Full-Name: {generalInfo.firstName + ' ' + generalInfo.lastName}</p>
+<p>E-mail: {generalInfo.mail}</p>
+<p>Phone: {generalInfo.phoneNumber} </p>
+<button onClick={handleGeneralEdit}>Edit</button>
 </div>
 </section>
+
 <section className='Educational-section'>
-<h1>General Info</h1>
-<form className='General-inputs' onSubmit={handleSubmit}>
+
+<h1>Educational expirience</h1>
+
+<form className='General-inputs' onSubmit={handleEducationalSubmit}>
   <div>
-    <SchoolName label="School-Name" handleChange={(e) => setSchoolName(e.target.value)}/>
+    <SchoolName label="School-Name" handleChange={(e) => setSchoolName(e.target.value)} value={schoolName}/>
   </div>
   <div>
-    <TitleOfStudy label="Title-Of-Study" handleChange={(e) => settitleOfStudy(e.target.value)}/>
+    <TitleOfStudy label="Title-Of-Study" handleChange={(e) => settitleOfStudy(e.target.value)} value={titleOfStudy}/>
   </div>
   <div>
-    <DateOfStudy label="Date-Of-Study" handleChange={(e) => setdateOfStudy(e.target.value)}/>
+    <DateOfStudy label="Date-Of-Study" handleChange={(e) => setdateOfStudy(e.target.value)} value={dateOfStudy}/>
   </div>
   <button type='submit'>Submit</button>
 </form>
@@ -62,6 +91,7 @@ function handleSubmit(e) {
 <p>School-Name: {educationInfo.name}</p>
 <p>Title-Of-Study: {educationInfo.title}</p>
 <p>Date-Of-Study: {educationInfo.date} </p>
+<button onClick={handleEducationalEdit}>Edit</button>
 </div>
 </section>
    </div>
